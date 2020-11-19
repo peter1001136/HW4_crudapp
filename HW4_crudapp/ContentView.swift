bf8013640fbc04c3de2c9ea79b7be442b74a0444
Peterchen <peter1001136@gmail.com>
2020-11-19 19:37:41 +0800

    Initial Commit

---
+++ HW4_crudapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW4_crudapp
+//
+//  Created by 陳政沂 on 2020/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW4_crudapp/HW4_crudappApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW4_crudappApp.swift
+//  HW4_crudapp
+//
+//  Created by 陳政沂 on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct HW4_crudappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
